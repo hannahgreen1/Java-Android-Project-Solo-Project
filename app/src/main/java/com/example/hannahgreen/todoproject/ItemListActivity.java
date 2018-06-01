a1e872d628be820fe3470e7628b7e22b6e88dabc
hannahgreen1 <hannahgreen94@icloud.com>
2018-06-01 14:14:31 +0100

    can view first page and tick boxes work

--- app/src/main/java/com/example/hannahgreen/todoproject/ItemListActivity.java
+++ app/src/main/java/com/example/hannahgreen/todoproject/ItemListActivity.java
@@ -1,13 +1,25 @@
 package com.example.hannahgreen.todoproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ItemListActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        ItemList itemList = new ItemList();
+        ArrayList<Item> list = itemList.getList();
+
+        ItemListAdapter itemAdapter = new ItemListAdapter(this, list);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
     }
 }
---
+++ app/src/main/java/com/example/hannahgreen/todoproject/ItemListAdapter.java
@@ -0,0 +1,39 @@
+package com.example.hannahgreen.todoproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+class ItemListAdapter extends ArrayAdapter<Item> {
+
+
+    public ItemListAdapter(Context context, ArrayList<Item> movies)
+    {
+        super(context, 0, movies);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent) {
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.item_activity, parent, false);
+        }
+
+        Item currentItem = getItem(position);
+
+        TextView title = (TextView) listItemView.findViewById(R.id.item_title);
+        title.setText(currentItem.getTitle().toString());
+
+
+        listItemView.setTag(currentItem);
+
+        return listItemView;
+
+    }
+}
