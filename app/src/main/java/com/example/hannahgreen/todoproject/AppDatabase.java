5a9ff82837dd07d49ffc233a0bcb76a5df14bc4e
hannahgreen1 <hannahgreen94@icloud.com>
2018-06-05 11:22:20 +0100

    DAO used and created

--- app/src/main/java/com/example/hannahgreen/todoproject/AppDatabase.java
+++ app/src/main/java/com/example/hannahgreen/todoproject/AppDatabase.java
@@ -1,10 +1,12 @@
 package com.example.hannahgreen.todoproject;
 
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
 class AppDatabase {
 
     @Database(entities = {Item.class}, version = 3)
     public abstract class AppDatabase extends RoomDatabase {
-        public abstract ArtistDao artistDao();
-        public abstract AlbumDao albumDao();
+        public abstract ItemDao itemDao();
     }
 }
---
+++ app/src/main/java/com/example/hannahgreen/todoproject/ItemDao.java
@@ -0,0 +1,8 @@
+package com.example.hannahgreen.todoproject;
+
+import android.arch.persistence.room.Dao;
+
+@Dao
+public interface ItemDao {
+    
+}
