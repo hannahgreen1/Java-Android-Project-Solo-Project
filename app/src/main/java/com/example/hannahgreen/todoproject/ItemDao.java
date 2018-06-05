af813679785873d31a20e949050449445ea5f8cb
hannahgreen1 <hannahgreen94@icloud.com>
2018-06-05 11:38:37 +0100

    DAO update

--- app/src/main/java/com/example/hannahgreen/todoproject/ItemDao.java
+++ app/src/main/java/com/example/hannahgreen/todoproject/ItemDao.java
@@ -1,8 +1,36 @@
 package com.example.hannahgreen.todoproject;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Dao
 public interface ItemDao {
+    @Query("SELECT * FROM item")
+    List<Item> getAll();
+
+    @Query("SELECT * FROM item where id = :id")
+    Item getById(int id);
+
+    @Query("SELECT * FROM item where title = :title")
+    Item getByTitle(String title);
+
+    @Insert
+    void insert(Item item);
+
+    @Update
+    void update(Item item);
+
+    @Delete
+    void delete(Item item);
+
+    @Insert
+    void deleteAll(Item... items);
+
 
 }
